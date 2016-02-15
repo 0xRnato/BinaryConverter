bfdcb44fbf882d43178e758b63f856c60aa725e6
Renato Neto <rnato.neto@hotmail.com>
2016-02-15 00:24:50 -0200

    Adição dos numeros 0 à esquerda

--- Program.cs
+++ Program.cs
@@ -6,26 +6,43 @@ namespace BinaryConverter
     {
         static void Main(string[] args)
         {
-            string key;
+            ulong bina = 0, casa = 1, resto, quo;
+            string binaStr = "", novabina = "";
+            int binaLength, multi = 0;
+
+            Console.Write("Enter a decimal number: ");
+            quo = ulong.Parse(Console.ReadLine());
+
             do
             {
-                ulong bina = 0, resto = 0, quo = 0, casa = 1;
+                resto = quo % 2;
+                bina += resto * casa;
+                quo /= 2;
+                casa *= 10;
+            } while (quo != 0);
 
-                Console.Write("Enter a decimal number: ");
-                quo = ulong.Parse(Console.ReadLine());
+            binaStr = Convert.ToString(bina);            
+            binaLength = binaStr.Length;
 
-                do
-                {
-                    resto = quo % 2;
-                    bina += resto * casa;
-                    quo /= 2;
-                    casa *= 10;
-                } while (quo != 0);
-                Console.WriteLine("Binary: " + bina);
-                Console.Write("\nEnter 'E' to exit... ");
-                key = Console.ReadLine();
-                Console.Clear();
-            } while (key != "e" && key != "E");
+            while (binaLength >= multi)
+            {
+                multi += 4;
+            }
+
+            for (int i = 0; i < multi-binaLength; i++)
+            {
+                novabina += "0";
+            }
+
+            for (int i = 0; i < binaLength; i++)
+            {
+                novabina += binaStr[i];
+            }
+
+            Console.WriteLine("Binary: " + bina);
+            Console.WriteLine("Binary: " + binaStr);
+            Console.WriteLine("Binary: " + novabina);
+            Console.ReadLine();
         }
     }
 }
