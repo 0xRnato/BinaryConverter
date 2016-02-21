c054bae6d82b28889eb2f025b58f734174e1d97c
Renato Neto <rnato.neto@hotmail.com>
2016-02-21 00:03:47 -0300

    Added binary to decimal conversion and a controlled menu by arrow keys

--- Program.cs
+++ Program.cs
@@ -4,8 +4,36 @@ namespace BinaryConverter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void BinaToDeci()
         {
+            Console.Clear();
+            string bina = "";
+            int[] deci;
+            int deciFinal = 0;
+
+            Console.Write("Enter a binary number: ");
+            bina = Console.ReadLine();
+            Console.Write("\n");
+
+            deci = new int[bina.Length];
+
+            for (int i = 0; i < deci.Length; i++)
+            {
+                deci[i] = Convert.ToInt32(bina[i].ToString());
+            }
+
+            for (int i = deci.Length; i > 0; i--)
+            {
+                deciFinal += deci[deci.Length - i] * (int)Math.Pow(2, i - 1);
+                //Step by step solving
+                Console.WriteLine(deci[deci.Length - i] + " * " + "2^" + (i - 1) + " = " + deci[deci.Length - i] * (int)Math.Pow(2, i - 1));
+            }
+
+            Console.WriteLine("\nDecimal: " + deciFinal);
+        }
+        static void DeciToBina()
+        {
+            Console.Clear();
             ulong bina = 0, casa = 1, resto, quo;
             string binaStr = "", binafinal = "";
             int multi = 0, nibbleCount = 0;
@@ -56,7 +84,46 @@ namespace BinaryConverter
             }
 
             Console.WriteLine("\nBinary: " + binafinal);
-            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            //navigation menu
+            int menu = 0;
+            Console.WriteLine("Decimal to Binary: X");
+            Console.WriteLine("Binary to Decimal:");
+            Console.WriteLine("\nUse UpArrow/DownArrow to navigate and press Enter to confirm.");
+
+            ConsoleKeyInfo input;
+            do
+            {
+                input = Console.ReadKey(true);
+
+                if (input.Key == ConsoleKey.UpArrow)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Decimal to Binary: X");
+                    Console.WriteLine("Binary to Decimal:");
+                    Console.WriteLine("\nUse UpArrow/DownArrow to navigate and press Enter to confirm.");
+                    menu = 0;
+                }
+                else if (input.Key == ConsoleKey.DownArrow)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Decimal to Binary:");
+                    Console.WriteLine("Binary to Decimal: X");
+                    Console.WriteLine("\nUse UpArrow/DownArrow to navigate and press Enter to confirm.");
+                    menu = 1;
+                }
+            } while (input.Key != ConsoleKey.Enter);
+
+            //DECIMAL TO BINATY
+            if (menu == 0)
+                DeciToBina();
+            //BINARY TO DECIMAL
+            else
+                BinaToDeci();
+
+            input = Console.ReadKey(true);
         }
     }
 }
